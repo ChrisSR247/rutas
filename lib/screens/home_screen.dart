76d7353589949e2f3ef0bb4c5dbdab670654b235
ChrisSR247 <nplok2690@gmail.com>
2025-12-01 20:10:54 -0500

    Proyecto

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -6,11 +7,35 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Home Screen')),
-      body: const Center(
+      appBar: AppBar(title: const Text('Home Page')),
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [Text('Esto es el Home', style: TextStyle(fontSize: 24))],
+          children: [
+            Icon(Icons.home, size: 88, color: Colors.amber),
+
+            SizedBox(height: 30),
+
+            Text('Welcome', style: TextStyle(fontSize: 24)),
+
+            ElevatedButton.icon(
+              onPressed: () {
+                Navigator.pushNamed(context, 'details');
+              },
+              label: Text('Ir a detalless'),
+              icon: Icon(Icons.info),
+            ),
+
+            SizedBox(height: 30),
+
+            ElevatedButton.icon(
+              onPressed: () {
+                Navigator.pushNamed(context, 'settings');
+              },
+              label: Text('Configuracion'),
+              icon: Icon(Icons.settings),
+            ),
+          ],
         ),
       ),
     );
